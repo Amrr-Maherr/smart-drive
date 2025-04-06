46ea288111363020c00a6bd382e8a06f8cb43304
Amr maher <amrm80233@>
2025-04-06 22:43:19 +0200

    add title and button to CoachEvaluationsRow

--- src/Components/CoachEvaluations/CoachEvaluationsRow.jsx
+++ src/Components/CoachEvaluations/CoachEvaluationsRow.jsx
@@ -1,13 +1,24 @@
 import React from "react"; // Import React
 import CoachEvaluationsBox from "./CoachEvaluationsCard"; 
-
+import ServicesText from "../ServicesSection/ServicesText"
 export default function CoachEvaluationsRow() {
   return (
     <>
-      <div className="flex items-start justify-center lg:justify-between w-[95%]  mx-auto flex-wrap gap-y-24 gap-x-4 pt-20 pb-10">
-          <CoachEvaluationsBox  />
-          <CoachEvaluationsBox  />
-          <CoachEvaluationsBox  />
+      <div className="w-[90%]  mx-auto">
+        <div className="flex items-center justify-between flex-row-reverse">
+          <ServicesText
+            title="تقييمات المدربين"
+            text="اراء العملا فى كل مدربين القياده"
+          />
+          <button className="p-[12px] border-[#1E3A8A] border rounded-lg">
+            اظهار الكل
+          </button>
+        </div>
+        <div className="flex items-start justify-center lg:justify-between  flex-wrap gap-y-24 gap-x-4  pb-10">
+          <CoachEvaluationsBox />
+          <CoachEvaluationsBox />
+          <CoachEvaluationsBox />
+        </div>
       </div>
     </>
   );
