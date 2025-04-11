70a5c50d5c35c52990b23be88436f87be40e672d
Amr maher <amrm80233@>
2025-04-11 17:20:53 +0200

    update

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,10 +1,12 @@
 import NavBar from "../Components/Nav/NavBar"
 import ProfileForm from "../Components/Profile/ProfileForm"
+import Footer from "../Components/Footer/Footer"
 export default function Profile() {
     return (
         <>
         <NavBar color="text-black"/>
         <ProfileForm/>
+        <Footer/>
         </>
     )
 }
