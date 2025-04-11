4f18fd1e8163e6c579e0e24c7ba5cb2ee13f4624
Amr maher <amrm80233@>
2025-04-11 15:49:02 +0200

    update

--- src/Components/BuyAndSell/Banner.jsx
+++ src/Components/BuyAndSell/Banner.jsx
@@ -4,7 +4,6 @@ export default function Banner({ Image }) {
       <div>
         <figure className=" relative overflow-hidden">
           <img src={Image} alt="" className="h-full w-full object-cover" />
-
           <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         </figure>
       </div>
