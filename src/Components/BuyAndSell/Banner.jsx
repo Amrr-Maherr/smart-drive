000cb33f3df10033173ca791ee6cb075a911944b
Amr maher <amrm80233@>
2025-04-11 17:35:55 +0200

    update style

--- src/Components/BuyAndSell/Banner.jsx
+++ src/Components/BuyAndSell/Banner.jsx
@@ -2,7 +2,7 @@ export default function Banner({ Image }) {
   return (
     <>
       <div>
-        <figure className=" relative overflow-hidden">
+        <figure className=" relative overflow-hidden h-[50dvh]">
           <img src={Image} alt="" className="h-full w-full object-cover" />
           <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         </figure>
